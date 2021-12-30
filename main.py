5a64c7d29bcba6aca3588762a8968e4bfb17a919
pirat-droid <aldegar80@gmail.com>
2021-12-30 15:24:52 +0500

    добавлен:  перевод заголовка на русский язы;  превращение заголовка в url;  поиск в базе тегов и добавление новых;  перевод тегов;

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import datetime
+import time
+import transliterate
+from mtranslate import translate
 import requests
 from bs4 import BeautifulSoup
+import connect_db
 
-def print_hi(name):
+
+def main():
+    cnh_db = connect_db.DbCNH()
     req = requests.get('https://blockchain.news/news/bakkt-president-adam-white-said-he-will-depart-from-bakkt-next-week')
     soup = BeautifulSoup(req.text, 'html.parser')
     element = soup.find('div', 'main col-sm-12')
-    title = element.find('h1', 'title21')
+    title = translate(element.find('h1', 'title21').text, 'ru')
+    print(title)
+    slug_t = transliterate.translit(title, reversed=True).replace(' ', '-')
+    print(slug_t)
+    time.sleep(5000)
     author = element.find("span", {"id": "author"})
     time_read = element.find('span', 'textdesc')
     print(title.text)
@@ -24,13 +35,20 @@ def print_hi(name):
             # сохраняем параграф
             print(p.text)
     tags = soup.find_all('button', 'btn tag-btn')
+    list_tags = []
     for t in tags:
-        # сохраняем теги
-        print(t.text)
+        # сохраняем теги если их не было в базе
+        list_tags.append(t.text)
+        if not cnh_db.select(f"SELECT ID FROM blog_tagmodel WHERE slug = '{t.text}'"):
+            # переводим теги
+            tag_name = translate(t.text, 'ru')
+            cnh_db.insert('''INSERT INTO blog_tagmodel (name, slug, datetime_create, datetime_update)
+                             VALUES (%s, %s, %s, %s)''',
+                          (tag_name, t.text, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                           datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            # лог записываем о добавлении нового тега
+            time.sleep(2)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
